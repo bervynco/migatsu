e92dfa739b3085147157a3f1e51fe70fac5402fc
Bervyn Co <bervyn_co2010@yahoo.com>
2017-11-14 17:11:24 +0800

    Updates on log_model

--- application/models/Log_model.php
+++ application/models/Log_model.php
@@ -1,15 +1,15 @@
 <?php
-    class log_model extends CI_Model {
-        function selectAllLogs(){
-            $query = $this->db->order_by("timestamp", "desc")->get('logs');
-            return($query->num_rows() > 0) ? $query->result_array(): array();
-        }
-
-        function insertLog($arrLog){
-            $query = $this->db->insert('logs', $arrLog);
-
-            return $this->db->insert_id();
-        }
+class log_model extends CI_Model {
+    function selectAllLogs(){
+        $query = $this->db->order_by("timestamp", "desc")->get('logs');
+        return($query->num_rows() > 0) ? $query->result_array(): array();
     }
+
+    function insertLog($arrLog){
+        $query = $this->db->insert('logs', $arrLog);
+
+        return $this->db->insert_id();
+    }
+}
 
 ?>
