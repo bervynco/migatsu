af6cffa275437fd4b8dbaf9f98c839aa1fea91e7
Bervyn Co <bervyn_co2010@yahoo.com>
2018-02-07 14:37:24 +0800

    select logs to ascending order

--- application/models/Log_model.php
+++ application/models/Log_model.php
@@ -1,7 +1,7 @@
 <?php
 class log_model extends CI_Model {
     function selectAllLogs(){
-        $query = $this->db->order_by("timestamp", "desc")->get('logs');
+        $query = $this->db->order_by("timestamp", "ASC")->get('logs');
         return($query->num_rows() > 0) ? $query->result_array(): array();
     }
 
